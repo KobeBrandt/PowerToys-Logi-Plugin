e050f78d46175cbee61ebe6f4cb96ffba4cf2566
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2026-04-02 16:11:35 +0200

    Added reading file

--- src/Actions/PowerToy.cs
+++ src/Actions/PowerToy.cs
@@ -3,19 +3,44 @@
 using Loupedeck;
 using Loupedeck.PowerToysPlugin;
 using Loupedeck.PowerToysPlugin.Helpers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 public abstract class PowerToy : ActionEditorCommand
 {
-    private readonly String _DefaultShortcut;
+    private String defaultShortcut;
     private readonly String _Icon;
     private readonly String _Name;
     private String image;
+
+    // Helper classes for JSON deserialization
+    public class PowerToysSettings
+    {
+        [JsonPropertyName("properties")]
+        public SettingsProperties Properties { get; set; }
+    }
+
+    public class SettingsProperties
+    {
+        [JsonPropertyName("ActivationShortcut")]
+        public ActivationShortcut ActivationShortcut { get; set; }
+    }
+
+    public class ActivationShortcut
+    {
+        [JsonPropertyName("win")] public Boolean Win { get; set; }
+        [JsonPropertyName("ctrl")] public Boolean Ctrl { get; set; }
+        [JsonPropertyName("alt")] public Boolean Alt { get; set; }
+        [JsonPropertyName("shift")] public Boolean Shift { get; set; }
+        [JsonPropertyName("code")] public Int32 Code { get; set; }
+        [JsonPropertyName("key")] public String Key { get; set; }
+    }
 
     public PowerToy(String name, String displayName, String shortcut, String groupName = null,
         String extraGroupName = null, String icon = null)
     {
         this._Name = name;
-        this._DefaultShortcut = shortcut;
+        // this.defaultShortcut = shortcut;
         this.DisplayName = displayName;
         this.GroupName = groupName;
         if (extraGroupName != null)
@@ -61,21 +86,99 @@ public abstract class PowerToy : ActionEditorCommand
         return BitmapHelper.MakeBitmapImage(this.image, imageWidth, imageHeight);
     }
 
-    private void OnControlValueChanged(Object sender, ActionEditorControlValueChangedEventArgs e) =>
+    private void OnControlValueChanged(Object sender, ActionEditorControlValueChangedEventArgs e)
+    {
+        PluginLog.Info("e");
         e.ActionEditorState.SetVisibility("CustomShortcut", false);
+        this.UpdateShortcutFromSettings();
+    }
+        
+
+    protected override Boolean OnLoad()
+    {
+        this.UpdateShortcutFromSettings();
+        return true;
+    }
+
+    private void UpdateShortcutFromSettings()
+    {
+        try
+        {
+            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var settingsPath = Path.Combine(localAppData, "Microsoft", "PowerToys", this._Name, "settings.json");
+
+            if (!File.Exists(settingsPath))
+            {
+                PluginLog.Info($"Settings file not found for {this._Name} at {settingsPath}");
+                return;
+            }
+
+            var jsonContent = File.ReadAllText(settingsPath);
+            var settings = JsonSerializer.Deserialize<PowerToysSettings>(jsonContent);
+
+            if (settings?.Properties?.ActivationShortcut != null)
+            {
+                var shortcutObj = settings.Properties.ActivationShortcut;
+                var shortcutStr = "";
+
+                if (shortcutObj.Win)
+                {
+                    shortcutStr += "Windows+";
+                }
+
+                if (shortcutObj.Ctrl)
+                {
+                    shortcutStr += "Ctrl+";
+                }
+
+                if (shortcutObj.Alt)
+                {
+                    shortcutStr += "Alt+";
+                }
+
+                if (shortcutObj.Shift)
+                {
+                    shortcutStr += "Shift+";
+                }
+
+                if (shortcutObj.Code > 0)
+                {
+                    if (shortcutObj.Code == 32)
+                    {
+                        shortcutStr += "Space";
+                    }
+                    else
+                    {
+                        var keyChar = (Char)shortcutObj.Code;
+                        shortcutStr += "Key" + char.ToUpper(keyChar);
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(shortcutStr))
+                {
+                    this.defaultShortcut = shortcutStr;
+                    PluginLog.Info($"Updated shortcut for {this._Name} to {this.defaultShortcut}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            PluginLog.Error(ex, $"Failed to update shortcut from settings for {this._Name}");
+        }
+    }
 
     protected override Boolean RunCommand(ActionEditorActionParameters actionParameters)
     {
         if (actionParameters.TryGetString("CustomShortcut", out var actionParameter))
         {
-            PluginLog.Info($"Sending shortcut: {actionParameter}");
+            PluginLog.Info($"{this._Name} | Sending shortcut: {actionParameter}");
             try
             {
                 // Use the built-in KeyboardShortcut command
-                var shortcut = String.IsNullOrEmpty(actionParameter) ? this._DefaultShortcut : actionParameter;
+                var shortcut = String.IsNullOrEmpty(actionParameter) ? this.defaultShortcut : actionParameter;
                 if (shortcut == "None___0______")
                 {
-                    shortcut = this._DefaultShortcut;
+                    shortcut = this.defaultShortcut;
                 }
 
 
