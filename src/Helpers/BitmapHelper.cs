7e79c8a21fa3e34541d8858a4b8932989cc4abdb
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2026-04-06 20:17:39 +0200

    Icons a bit bigger

--- src/Helpers/BitmapHelper.cs
+++ src/Helpers/BitmapHelper.cs
@@ -4,16 +4,16 @@ public static class BitmapHelper
 {
     public static BitmapImage MakeBitmapImage(String path, PluginImageSize imageSize)
     {
-        var scale = 2;
+        var scale = 1.5;
         try
         {
             using var builder = new BitmapBuilder(imageSize);
             var cw = builder.Width;
             var ch = builder.Height;
-            var dw = cw / scale;
-            var dh = ch / scale;
-            var dx = (cw - dw) / scale;
-            var dy = (ch - dh) / scale;
+            Int32 dw = (int) (cw / scale);
+            var dh = (int)(ch / scale);
+            var dx = (int)((cw - dw) / scale);
+            var dy = (int)((ch - dh) / scale);
             builder.DrawImage(EmbeddedResources.ReadImage(path), dx, dy, dw, dh);
             return builder.ToImage();
         }
